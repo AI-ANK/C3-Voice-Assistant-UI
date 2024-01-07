d7207436ae7cf32721f1c796953f220687fa1637
AI-ANK <130707158+AI-ANK@users.noreply.github.com>
2024-01-07 14:10:49 +0000

    added header

--- src/App.js
+++ src/App.js
@@ -147,7 +147,7 @@ const App = () => {
       <div class="app-header">
           <h1>C3 AI</h1>
           <p class="app-subtext">
-            Ask me anything, or get specific insights from the Nvidia FYE 2023 10K
+            Ask me anything, or get specific insights from the Nvidia FY 2023 10K
             report.
           </p>
           <p class="app-subtext">Just say 'C3' to activate me!</p>
